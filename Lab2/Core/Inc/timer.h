615481456f7b603a193c1a2ab965b37e74884f18
inori2804 <congnguyenvan002@gmail.com>
2022-10-10 15:53:40 +0700

    commit0

---
+++ Lab2/Core/Inc/timer.h
@@ -0,0 +1,17 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 9, 2022
+ *      Author: shiba
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+
+#endif /* INC_TIMER_H_ */
